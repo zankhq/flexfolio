cd3320e04659a1aa3704d0373954ce3d488fed99
Giulio Zanchetta <zank@outlook.it>
2023-09-09 02:17:06 +0200

    feat: Add AstroPWA configuration for production mode
    This commit adds the AstroPWA configuration for production mode in the astro.config.mjs file. The configuration includes settings such as the base URL, scope, included assets, registration type, and manifest details. It also configures workbox options and development options.
    
    fix: Adjust image dimensions in Blob component
    
    The commit adjusts the height and width attributes of an image in the Blob.astro component to ensure proper rendering. The image now has a height of 200 pixels and a width of 600 pixels.
    
    refactor: Update Navbar component with click event handling
    
    This commit updates the Navbar.astro component by adding click event handling to toggle between portfolioOpen and aboutOpen states when clicking on the logo.
    
    chore: Uncomment PWA related code in Layout component
    
    The commit uncommented previously commented out code related to PWA (Progressive Web App) functionality in the Layout.astro component. It includes importing pwaInfo from virtual:pwa-info and adding it to the HTML fragment set.
    
    chore: Include pwa.ts script in Layout component
    
    This commit adds a script tag that references "/src/pwa.ts" to include it in the Layout.astro component's HTML structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import NetlifyCMS from "astro-netlify-cms";
 import alpinejs from "@astrojs/alpinejs";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -95,6 +96,45 @@ export default defineConfig({
 				],
 			},
 			disableIdentityWidgetInjection: true,
+		}),
+		AstroPWA({
+			mode: "production",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Flexfolio - Starter Template for Astro with Tailwind CSS",
+				short_name: "Flexfolio",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["*.js"],
+			},
+			devOptions: {
+				enabled: true,
+				navigateFallbackAllowlist: [/^\/404$/],
+				suppressWarnings: true,
+			},
 		}),
 		alpinejs(),
 	],
